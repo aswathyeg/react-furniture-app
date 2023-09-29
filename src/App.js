7cc6e32295bc05c73505b40af822f4601e0de07f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-09-29 08:10:15 +0530

    added nested cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+// App.js
+
+import React from "react";
+import Card from "./components/Card";
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card>
+        <h1>Parent Card</h1>
+        <p>This is the parent card content.</p>
+
+        <Card>
+          <h2>Child Card</h2>
+          <p>This is the child card content.</p>
+        </Card>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
