5084df5ec401e3d24e5e237653a5bcb96ed45da9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-10-05 23:05:51 +0530

    added props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Card.css";
-const Card = ({ children }) => {
-  return <div className="card">{children}</div>;
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <img src={props.img} className="card-image" />
+        <h2 className="card-title">{props.title}</h2>
+        <p className="card-description">{props.text}</p>
+        <button className="card-btn">View product</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
