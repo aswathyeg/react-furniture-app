10dc2761922462151785b3ac66c45f1b340af6ba
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-10-06 20:25:30 +0530

    added more cards

--- src/App.js
+++ src/App.js
@@ -6,26 +6,54 @@ import Card from "./components/Card";
 import blue from "./assets/blue.jpg";
 import white from "./assets/white.jpg";
 import yellow from "./assets/yellow.jpg";
+import white2 from "./assets/white2.jpg";
+import orange2 from "./assets/orange2.jpg";
+import wood from "./assets/wood.jpg";
+import NewNavbar from "./components/NewNavbar";
 function App() {
   return (
-    <div className="wrapper">
-      <Card
-        img={blue}
-        title="Style-blue"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do."
-      />
-      <Card
-        img={white}
-        title="Style-white"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+    <div>
+      <NewNavbar />
+      <div className="navtop"></div>
+      <div className="wrapper">
+        <Card
+          img={blue}
+          title="Style-blue"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ."
+        />
+        <Card
+          img={white}
+          title="Style-white"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
           "
-      />
-      <Card
-        img={yellow}
-        title="Style-yellow"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        />
+        <Card
+          img={yellow}
+          title="Style-yellow"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed.
           "
-      />
+        />
+      </div>
+      <div className="navtop"></div>
+      <div className="wrapper">
+        <Card
+          img={orange2}
+          title="Style-orange"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ."
+        />
+        <Card
+          img={white2}
+          title="Style-white"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .
+          "
+        />
+        <Card
+          img={wood}
+          title="Style-wooden"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .
+          "
+        />
+      </div>
     </div>
   );
 }
