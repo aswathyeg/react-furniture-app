32bda6c9f26434eef1e77098101ca94b4155eeed
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-10-06 22:15:50 +0530

    added cart icon

--- src/components/NewNavbar.js
+++ src/components/NewNavbar.js
@@ -1,19 +1,21 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import { PiShoppingCartFill } from "react-icons/pi";
 const NewNavbar = () => {
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light" className="nb">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/" className="newNavBrand">
             Happy Heels
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/writings">About</Nav.Link>
+            <Nav.Link href="/writings" className="about">
+              About
+            </Nav.Link>
             <Nav.Link href="/works">
-              <PiShoppingCartFill />
+              <PiShoppingCartFill fill="blue" />
             </Nav.Link>
             {/* <Nav.Link href="/skills">Skills</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link> */}
