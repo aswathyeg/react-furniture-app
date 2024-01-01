12101a453a6685082dfa4f82fa7fdb6c3da330e9
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-01 15:55:41 +0530

    added props

---
+++ src/components/ProductCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card';
+const ProductCard = ({products}) => {
+  return (
+    <div > 
+  
+    <Card products={products}/>
+    </div>
+   
+  )
+}
+
+export default ProductCard
