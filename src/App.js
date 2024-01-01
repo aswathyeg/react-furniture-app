5b6063c51084f246785c2fac7e5e5316b68bd48b
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-01 15:41:36 +0530

    updated image

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ import NewNavbar from "./components/NewNavbar";
 import FilterableProductCard from "./components/FilterableProductCard";
 function App() {
   let PRODUCTS=[
-    {img:{white},title:"Style-white",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+    {id:1,imageUrl:white,title:"Style-white",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
     price:"$2",stocked:true
   },
-  {img:{blue},title:"Style-blue",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+  {id:2,imageUrl:blue,title:"Style-blue",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
   price:"$2",stocked:true
 },
-{img:{yellow},title:"Style-yellow",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+{id:3,imageUrl:yellow,title:"Style-yellow",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
 price:"$2",stocked:true
 },
-{img:{orange2},title:"Style-orange",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+{id:4,imageUrl:orange2,title:"Style-orange",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
 price:"$2",stocked:true
 },
-{img:{white2},title:"Style-white",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+{id:5,imageUrl:white2,title:"Style-white",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
 price:"$2",stocked:true
 },
-{img:{wood},title:"Style-wooden",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
+{id:6,imageUrl:wood,title:"Style-wooden",text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .",
 price:"$2",stocked:true
 },
   ]
@@ -36,7 +36,7 @@ price:"$2",stocked:true
     
   return (
     <div>
-      <NewNavbar />
+      
       {/* <div className="navtop"></div>
       <div className="wrapper">
         <Card
