3128558d481807c27f62bd5d8b4ad2a20ed71a8c
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-19 22:59:34 +0530

    updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-// import "./Card.css";
+import "./Card.css";
 import Card from 'react-bootstrap/Card';
 const Cards = ({products}) => {
+  const [renderedCardIds,setRenderdCardIds]=useState([])
   
   return (
     <div>
       {products?.map(item=>(
+        
         <Container>
-    <Card>
-            <Card.Img variant="top" key={item.id} src={item.imageUrl} />
+          
+          
+    <Card key={item.id} >
+            <Card.Img variant="top" key={item.id} src={item.imageUrl}
+             style={{ maxWidth: '100%', height: '20%' }} />
             <Card.Body>
               <Card.Title>{item.title}</Card.Title>
               <Card.Text>
